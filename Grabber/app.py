b05e90bbad7d09d9a8c0dbf9736dfc6e8c9e2370
bdvllrs <bdvllrs@gmail.com>
2017-08-25 15:36:57 +0200

    bug fix

--- Grabber/app.py
+++ Grabber/app.py
@@ -24,6 +24,7 @@ while True:
                 camera.clear(pictures_memory)
                 recording = int(requests.get(api_url + '/setting/recording').json()['content'])
                 sleep(photo_interval)
+    sleep(5)
 
 
 
