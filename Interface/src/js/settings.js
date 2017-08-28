dfd8074222169c04ca9444b3ef169f920a82d169
bdvllrs <bdvllrs@gmail.com>
2017-08-28 11:54:53 +0200

    use public url

--- Interface/src/js/settings.js
+++ Interface/src/js/settings.js
@@ -1,4 +1,5 @@
-export const publicUrl = 'http://192.168.0.31:8080';
+// export const publicUrl = 'http://192.168.0.31:8080';
+export const publicUrl = 'http://78.244.38.129:8080';
 // export const publicUrl = 'http://127.0.0.1:8000';
 export const apiUrl = publicUrl + '/api';
 export const refreshInterval = 3000;
